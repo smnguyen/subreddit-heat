714f8509cee3a5a5713deaff3ce07ad8fb59a8db
Sammy Nguyen <sammy.nguyen@gusto.com>
2017-07-23 22:28:24 -0700

    Refactor for upcoming states

--- src/handlers/new_session.js
+++ src/handlers/new_session.js
@@ -1,6 +1,28 @@
 import { fetchPosts, getSubreddit } from 'helpers';
 
-const SkillHandlers = {
+const NewSessionHandlers = {
+  'AMAZON.CancelIntent': function() {
+    this.emit(':tell', 'OK!');
+  },
+  'AMAZON.HelpIntent': function() {
+    this.emit(
+      ':ask',
+      'I can tell you the hot posts for a subreddit you care about.',
+      'Here\'s an example: ask me, "What are the hot posts on /r/politics?"'
+    );
+  },
+  'AMAZON.NextIntent': function() {
+    this.emit('AMAZON.HelpIntent');
+  },
+  'AMAZON.PreviousIntent': function() {
+    this.emit('AMAZON.HelpIntent');
+  },
+  'AMAZON.RepeatIntent': function() {
+    this.emit('AMAZON.HelpIntent');
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit('AMAZON.CancelIntent');
+  },
   'HotPostsIntent': function() {
     const request = this.event.request;
     const { subreddit, query } = getSubreddit(request);
@@ -25,7 +47,10 @@ const SkillHandlers = {
         console.error(error);
         console.error(`Failed to fetch posts for query ${query}, subreddit ${subreddit}`);
       });
+  },
+  'Unhandled': function() {
+    this.emit('AMAZON.HelpIntent');
   }
 };
 
-export default SkillHandlers;
+export default NewSessionHandlers;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import Alexa from 'alexa-sdk';
 
-import AmazonHandlers from 'handlers/amazon';
-import SkillHandlers from 'handlers/skill';
+import NewSessionHandlers from 'handlers/new_session';
 
 export function handler(event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
   alexa.APP_ID = process.env.APP_ID;
-  alexa.registerHandlers(AmazonHandlers, SkillHandlers);
+  alexa.registerHandlers(NewSessionHandlers);
   alexa.execute();
 }
