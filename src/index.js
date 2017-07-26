993d43daa4ac70963fe6e7dc7ad4ebdb9155f353
Sammy Nguyen <sammy.nguyen@gusto.com>
2017-07-25 22:49:21 -0700

    Fix app id

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import PostDetailHandlers from 'handlers/post_detail';
 
 export function handler(event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
-  alexa.APP_ID = process.env.APP_ID;
+  alexa.appId = process.env.APP_ID;
   alexa.registerHandlers(
     NewSessionHandlers,
     HotPostsHandlers,
